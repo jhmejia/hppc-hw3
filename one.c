e05e96a881445de68595eac646697c7a7cf97118
JHMejia <29128358+jhmejia@users.noreply.github.com>
2024-02-26 22:08:35 -0600

    String one

--- one.c
+++ one.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <mpi.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     int rank, size;
@@ -10,7 +11,55 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Get the rank of the process
     MPI_Comm_size(MPI_COMM_WORLD, &size); // Get the number of processes
 
-    printf("Hello World from process %d of %d\n", rank, size);
+    if (size < 2) {
+        printf("This program requires at least 2 processes\n");
+        MPI_Finalize();
+        return 1;
+    }
+
+    if (rank == 0)
+        printf("===Part 1 ===\n");
+
+    // Build the message 
+    char message[100];
+    char hostname[100];
+    gethostname(hostname, 100);
+    sprintf(message, "Greetings from process %d of %d on %s", rank, size, hostname);
+
+    char recv_message[100];
+
+    // Barrier to synchronize all processes
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Create a for loop in which process 0 recieves the message from all other processes in order and prints them. Note only process 0 will print the messages
+    if (rank == 0) {
+        printf("%s\n", message);
+
+        for (int i = 1; i < size; i++) {
+            MPI_Recv(recv_message, 100, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("%s\n", recv_message);
+        }
+    } else {
+        MPI_Send(message, 100, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+
+    if (rank == 0)
+        printf("\n===Part 2 ===\n");
+    // Barrier
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    
+    // Create a for loop in which process 0 recieves the message all other process in any order and prints them. Note only process 0 will print the messages
+    if (rank == 0) {
+        MPI_Send(message, 100, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+
+        for (int i = 0; i < size; i++) {
+            MPI_Recv(recv_message, 100, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("%s\n", recv_message);
+        }
+    } else {
+        MPI_Send(message, 100, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
 
     MPI_Finalize(); // Finalize MPI
     return 0;
